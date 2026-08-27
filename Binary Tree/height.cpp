bb3471f8df29883d23367ba75afc24540c6adead
Rohit Rawat <rawatrohit12386@gmail.com>
2026-08-27 19:24:32 +0530

    height of binary tree

---
+++ Binary Tree/height.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+static int idx = -1;
+
+Node* buildTree(vector<int>& nodes) {
+
+    idx++;
+
+    if (nodes[idx] == -1) {
+        return NULL;
+    }
+
+    Node* currNode = new Node(nodes[idx]);
+
+    currNode->left = buildTree(nodes);
+    currNode->right = buildTree(nodes);
+
+    return currNode;
+}
+  int height(Node* root){
+    if(root == NULL){
+        return 0;
+    }
+    return 1+max(height(root->left) , height(root->right));
+  }
+int main() {
+
+    vector<int> nodes = {
+        1, 2, 4, -1, -1, 5, -1, -1,
+        3, -1, 6, -1, -1
+    };
+    Node* root = buildTree(nodes);
+   cout << height(root);
+    return 0;
+}
