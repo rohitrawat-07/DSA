3acb6f02f0b9ea893ca3df3a8e28c85f0bc66809
Rohit Rawat <rawatrohit12386@gmail.com>
2026-08-28 16:01:37 +0530

    top view of a binary tree

---
+++ Binary Tree/topView.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <vector>
+#include<queue>
+#include<map>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+static int idx = -1;
+
+Node* buildTree(vector<int>& nodes) {
+
+    idx++;
+
+    if (nodes[idx] == -1) {
+        return NULL;
+    }
+
+    Node* currNode = new Node(nodes[idx]);
+
+    currNode->left = buildTree(nodes);
+    currNode->right = buildTree(nodes);
+
+    return currNode;
+}
+  void topView(Node* root){
+    queue<pair<Node* , int>> q; // node and horizontal distance
+    map<int,int> m;
+    q.push(make_pair(root , 0));
+    while(!q.empty()){
+    pair<Node* , int> curr = q.front();
+    q.pop();
+    Node* currnode = curr.first;
+    int dist = curr.second;
+    if(m.count(dist) == 0){
+     m[dist] = currnode->data;
+    }
+    if(currnode->left != NULL){
+        pair<Node* , int> left = make_pair(currnode->left , dist-1);
+        q.push(left);
+    }
+    if(currnode->right != NULL){
+        pair<Node* , int> right = make_pair(currnode->right , dist+1);
+        q.push(right);
+    }
+    }
+    for(auto it :m){
+    cout << it.second << " ";
+  }
+
+  }
+int main() {
+
+    vector<int> nodes = {
+        1, 2, 4, -1, -1, 5, -1, -1,
+        3, -1, 6, -1, -1
+    };
+    Node* root = buildTree(nodes);
+    topView(root);
+    return 0;
+}
